b0e30dbe1f719e8d6756e3509613b842c6413500
Julia Fideles <julia.hfideles@gmail.com>
2023-10-19 15:43:00 -0300

    feature/Image-resize

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# image-resize
+# Image Resize Console Application
+
+This is a simple console application for resizing images. It allows you to easily resize your images to a specified size.
+
+## Instructions
+
+Follow these steps to use the application:
+
+1. **Download the Project:**
+   - Clone or download this project to your local machine.
+
+2. **Run the Project:**
+   - Open the project in your favorite C# development environment (e.g., Visual Studio, Visual Studio Code).
+
+3. **Locate the Input Directory:**
+   - After running the project, you will find the `input_directory` folder at the following path: `image-resize\redimensionador\bin\Debug\net6.0\`.
+
+4. **Choose the Desired Image Size:**
+   - Place the images you want to resize into the `input_directory` folder.
+
+5. **Run the Application:**
+   - Start the application, and it will periodically check the `input_directory` for images to resize. You can specify the desired size in the code.
+
+6. **Access Resized Images:**
+   - The resized images will be saved in the `resize_directory` folder.
+
+7. **Repeat as Needed:**
+   - You can repeat this process to resize additional images as required.
+
+## Note
+
+- Ensure that you have .NET 7.0 or a compatible runtime installed on your system to run this application.
+
+- Be aware of the file naming and overwriting rules. If there are existing files with the same name in the `resize_directory`, they might be overwritten.
+
+Enjoy using the Image Resize Console Application! If you encounter any issues or have suggestions for improvements, feel free to open an issue or contribute to the project.
---
+++ redimensionador/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using System.Threading;
+
+Thread thread = new Thread(Resize);
+thread.Start();
+
+
+static void Resize()
+{
+    #region "Directories"
+    string input_directory = "input_directory";
+    string resizer_directory = "resize_directory";
+
+    if (!Directory.Exists(input_directory) || !Directory.Exists(resizer_directory))
+    {
+        Directory.CreateDirectory(input_directory);
+        Directory.CreateDirectory(resizer_directory);
+    }
+    #endregion
+
+
+    while (true)
+    {
+        var input_files = Directory.EnumerateFiles(input_directory);
+
+        Console.WriteLine("Adicione a imagem que deseja redimensionar na pasta input_directory");
+        Console.WriteLine("Informe o tamanho desejado para a nova imagem");
+        var size = Console.ReadLine();
+
+        foreach (var file in input_files)
+        {
+            FileStream fileStream;
+            FileInfo fileInfo;
+
+            fileStream = new FileStream(file, FileMode.Open,FileAccess.ReadWrite,FileShare.ReadWrite);
+            fileInfo = new FileInfo(file);
+            string end_Directory = Environment.CurrentDirectory + @"\" + 
+                resizer_directory + @"\resize_" + size + "_" + fileInfo.Name;
+
+            ResizingImage(Image.FromStream(fileStream), Convert.ToInt32(size), end_Directory);
+
+        }
+        Thread.Sleep(new TimeSpan(0,0,5));
+    }
+}
+
+
+static void ResizingImage(Image image, int height, string directory)
+{
+    double ratio = (double)height / image.Height;
+    int newWidth = (int)(image.Width * ratio);
+    int newHeight = (int)(image.Height * ratio);
+
+    Bitmap newImage;
+    newImage = new Bitmap(newWidth, newHeight);
+
+    using (Graphics graphics = Graphics.FromImage(newImage))
+    {
+        graphics.DrawImage(image,0,0, newWidth, newHeight);
+    }
+
+    newImage.Save(directory);
+    image.Dispose();
+}
